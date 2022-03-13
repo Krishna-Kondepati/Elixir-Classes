05dc16b5dd5893b6ad92d92b19d2e102010eae32
KDNKrishna <kdnkrishna@gmail.com>
2022-03-13 20:38:45 +0100

    Add files via upload

---
+++ hello_world.exs
@@ -0,0 +1,11 @@
+defmodule HelloWorld do
+  @doc """
+  Simply returns "Hello, World!"
+  """
+  @spec hello :: String.t()
+  def hello do
+    IO.puts "Hello, World!"
+  end
+end
+
+HelloWorld.hello
