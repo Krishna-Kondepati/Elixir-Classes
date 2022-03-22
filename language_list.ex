d2c643d47c84651fc815024c0e2a7b1df438af7a
Krishna-Kondepati <71731508+Krishna-Kondepati@users.noreply.github.com>
2022-03-22 21:50:19 +0100

    Update language_list.ex

--- language_list.ex
+++ language_list.ex
@@ -1,31 +1,54 @@
 defmodule LanguageList do
+
+  @doc """
+  Implementing the new/0 function which takes no argument and returns an empty list.
+  """
+  @spec new() :: []
   def new() do
-    # Please implement the new/0 function
     []
   end
 
+  @doc """
+  Implementing the add/2 function which takes no argument and
+  returns the resulting list with the new language prepended to the given list.
+  """
+  @spec add(list(), String) :: list(String.t())
   def add(list, language) do
-    # Please implement the add/2 function
     [language | list]
   end
 
+  @doc """
+  Implementing the remove/1 function which takes 1 argument and returns he list without the first item.
+  Assume the list will always have at least one item.
+  """
+  @spec remove(list()) :: list(String.t())
   def remove(list) do
-    # Please implement the remove/1 function
     List.delete_at(list, 0)
   end
 
+  @doc """
+  Implementing the first/1 function which takes 1 argument and returns the first language in the list.
+  """
+  @spec first(list()) :: list(String.t())
   def first(list) do
-    # Please implement the first/1 function
     List.first(list)
   end
 
+  @doc """
+  Implementing the count/1 function which takes 1 argument and returns the number of languages in the list.
+  """
+  @spec count(list()) :: integer()
   def count(list) do
-    # Please implement the count/1 function
     Enum.count(list)
   end
 
+  @doc """
+  Implementing the exciting_list?/1 function which takes 1 argument and returns a boolean value.
+  It should return true if "Elixir" is one of the languages in the list.
+  """
+  @spec exciting_list?(list()) :: boolean()
   def exciting_list?(list) do
-    # Please implement the exciting_list?/1 function
+    # Please implement
     Enum.member?(list, "Elixir")
   end
 end
